a0b92399e789852ed335685ca2720a2488b6efd8
javaqsq <1113948520@qq.com>
2020-02-01 19:49:06 +0800

    coding

---
+++ qsq-blog/src/main/java/com/qsq/qsqblog/QsqBlogApplication.java
@@ -0,0 +1,16 @@
+package com.qsq.qsqblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qsq
+ */
+@SpringBootApplication
+public class QsqBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QsqBlogApplication.class, args);
+    }
+
+}
