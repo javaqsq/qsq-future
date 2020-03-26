0bca5346337468704caed83b0393775ad9dd3fe6
javaqsq <1113948520@qq.com>
2020-03-26 22:40:31 +0800

    coding

--- qsq-common/src/main/java/com/qsq/common/uitl/CollectionUtils.java
+++ qsq-common/src/main/java/com/qsq/common/uitl/CollectionUtils.java
@@ -5,7 +5,14 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.springframework.beans.BeanUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
